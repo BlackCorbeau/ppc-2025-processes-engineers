47bbd92f941b40db38f9b11a0e54eb2d63f811cb
Kirill <kremizov3@gmail.com>
2025-10-29 22:37:10 +0300

    feat(tasks/remizov_k_max_in_matrix_string): Initialize Workdir sample
    I mean that all namespaces, classes, methods that need to be renamed has been renamed according of documentation

---
+++ tasks/remizov_k_max_in_matrix_string/seq/src/ops_seq.cpp
@@ -0,0 +1,60 @@
+#include "remizov_k_max_in_matrix_string/seq/include/ops_seq.hpp"
+
+#include <numeric>
+#include <vector>
+
+#include "example_processes/common/include/common.hpp"
+#include "util/include/util.hpp"
+
+namespace remizov_k_max_in_matrix_string{
+
+RemizovKMaxInMatrixStringSEQ::RemizovKMaxInMatrixStringSEQ(const InType &in) {
+  SetTypeOfTask(GetStaticTypeOfTask());
+  GetInput() = in;
+  GetOutput() = 0;
+}
+
+bool RemizovKMaxInMatrixStringSEQ::ValidationImpl() {
+  return (GetInput() > 0) && (GetOutput() == 0);
+}
+
+bool RemizovKMaxInMatrixStringSEQ::PreProcessingImpl() {
+  GetOutput() = 2 * GetInput();
+  return GetOutput() > 0;
+}
+
+bool RemizovKMaxInMatrixStringSEQ::RunImpl() {
+  if (GetInput() == 0) {
+    return false;
+  }
+
+  for (InType i = 0; i < GetInput(); i++) {
+    for (InType j = 0; j < GetInput(); j++) {
+      for (InType k = 0; k < GetInput(); k++) {
+        std::vector<InType> tmp(i + j + k, 1);
+        GetOutput() += std::accumulate(tmp.begin(), tmp.end(), 0);
+        GetOutput() -= i + j + k;
+      }
+    }
+  }
+
+  const int num_threads = ppc::util::GetNumThreads();
+  GetOutput() *= num_threads;
+
+  int counter = 0;
+  for (int i = 0; i < num_threads; i++) {
+    counter++;
+  }
+
+  if (counter != 0) {
+    GetOutput() /= counter;
+  }
+  return GetOutput() > 0;
+}
+
+bool RemizovKMaxInMatrixStringSEQ::PostProcessingImpl() {
+  GetOutput() -= GetInput();
+  return GetOutput() > 0;
+}
+
+}  // namespace remizov_k_max_in_matrix_string
