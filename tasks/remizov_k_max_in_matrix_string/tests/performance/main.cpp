add57927d34fa21725aeaf82402c2bc61dad996d
Kirill <kremizov3@gmail.com>
2025-11-09 19:44:26 +0300

    feat(tasks/remizov_k_max_in_matrix_string/tests/performance/main.cpp): Initialize perfomance tests

--- tasks/remizov_k_max_in_matrix_string/tests/performance/main.cpp
+++ tasks/remizov_k_max_in_matrix_string/tests/performance/main.cpp
@@ -1,22 +1,31 @@
 #include <gtest/gtest.h>
 
+#include <vector>
+#include <iostream>
+
 #include "remizov_k_max_in_matrix_string/common/include/common.hpp"
 #include "remizov_k_max_in_matrix_string/mpi/include/ops_mpi.hpp"
 #include "remizov_k_max_in_matrix_string/seq/include/ops_seq.hpp"
 #include "util/include/perf_test_util.hpp"
 
-namespace remizov_k_max_in_matrix_string{
+namespace remizov_k_max_in_matrix_string {
 
-class ExampleRunPerfTestProcesses : public ppc::util::BaseRunPerfTests<InType, OutType> {
-  const int kCount_ = 100;
+class RemizovKRunPerfMaxInMatrixString : public ppc::util::BaseRunPerfTests<InType, OutType> {
   InType input_data_{};
+  OutType expected_output_{};
 
   void SetUp() override {
-    input_data_ = kCount_;
+    input_data_ = {
+      {1, 2, 3},
+      {4, 5, 6},
+      {7, 8, 9}
+    };
+
+    expected_output_ = {3, 6, 9};
   }
 
   bool CheckTestOutputData(OutType &output_data) final {
-    return input_data_ == output_data;
+    return output_data == expected_output_;
   }
 
   InType GetTestInputData() final {
@@ -24,17 +33,17 @@ class ExampleRunPerfTestProcesses : public ppc::util::BaseRunPerfTests<InType, O
   }
 };
 
-TEST_P(ExampleRunPerfTestProcesses, RunPerfModes) {
+TEST_P(RemizovKRunPerfMaxInMatrixString, RunPerfModes) {
   ExecuteTest(GetParam());
 }
 
 const auto kAllPerfTasks =
-    ppc::util::MakeAllPerfTasks<InType, RemizovKMaxInMatrixStringMPI, RemizovKMaxInMatrixStringMPI>(PPC_SETTINGS_example_processes);
+    ppc::util::MakeAllPerfTasks<InType, RemizovKMaxInMatrixStringMPI, RemizovKMaxInMatrixStringSEQ>(PPC_SETTINGS_remizov_k_max_in_matrix_string);
 
 const auto kGtestValues = ppc::util::TupleToGTestValues(kAllPerfTasks);
 
-const auto kPerfTestName = ExampleRunPerfTestProcesses::CustomPerfTestName;
+const auto kPerfTestName = RemizovKRunPerfMaxInMatrixString::CustomPerfTestName;
 
-INSTANTIATE_TEST_SUITE_P(RunModeTests, ExampleRunPerfTestProcesses, kGtestValues, kPerfTestName);
+INSTANTIATE_TEST_SUITE_P(RunModeTests, RemizovKRunPerfMaxInMatrixString, kGtestValues, kPerfTestName);
 
 }  // namespace remizov_k_max_in_matrix_string
