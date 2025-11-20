4d4b4c279c9d4de92fea54b6704edca0eb34ba60
Kirill <remizov.kirill2005@yandex.ru>
2025-11-20 21:45:09 +0300

    fix(SEQ): rethinking of SEQ

--- tasks/remizov_k_max_in_matrix_string/seq/src/ops_seq.cpp
+++ tasks/remizov_k_max_in_matrix_string/seq/src/ops_seq.cpp
@@ -1,7 +1,6 @@
 #include "remizov_k_max_in_matrix_string/seq/include/ops_seq.hpp"
 
 #include <algorithm>
-#include <cstddef>
 #include <vector>
 
 #include "remizov_k_max_in_matrix_string/common/include/common.hpp"
@@ -16,19 +15,16 @@ RemizovKMaxInMatrixStringSEQ::RemizovKMaxInMatrixStringSEQ(const InType &in) {
 
 bool RemizovKMaxInMatrixStringSEQ::ValidationImpl() {
   if (GetInput().empty()) {
-    return false;
+    return true;
   }
 
-  if (!GetOutput().empty()) {
-    return false;
+  size_t first_row_size = GetInput()[0].size();
+  for (const auto &row : GetInput()) {
+    if (row.size() != first_row_size) {
+      return false;
+    }
   }
-
-  const std::size_t first_row_size = GetInput()[0].size();
-  if (first_row_size == 0) {
-    return false;
-  }
-
-  return std::ranges::all_of(GetInput(), [first_row_size](const auto &row) { return row.size() == first_row_size; });
+  return true;
 }
 
 bool RemizovKMaxInMatrixStringSEQ::PreProcessingImpl() {
@@ -37,44 +33,19 @@ bool RemizovKMaxInMatrixStringSEQ::PreProcessingImpl() {
 }
 
 bool RemizovKMaxInMatrixStringSEQ::RunImpl() {
+  GetOutput().reserve(GetInput().size());
+
   for (const auto &row : GetInput()) {
     if (!row.empty()) {
-      int max_element = *std::ranges::max_element(row);
-      GetOutput().push_back(max_element);
-    }
-  }
-
-  return !GetOutput().empty();
-}
-
-bool RemizovKMaxInMatrixStringSEQ::PostProcessingImpl() {
-  if (GetOutput().size() != GetInput().size()) {
-    return false;
-  }
-
-  for (std::size_t i = 0; i < GetOutput().size(); i++) {
-    const auto &row = GetInput()[i];
-    int found_max = GetOutput()[i];
-
-    bool max_exists = false;
-    for (int value : row) {
-      if (value == found_max) {
-        max_exists = true;
-        break;
-      }
-    }
-
-    if (!max_exists) {
-      return false;
-    }
-    for (int value : row) {
-      if (value > found_max) {
-        return false;
-      }
+      GetOutput().push_back(*std::max_element(row.begin(), row.end()));
     }
   }
 
   return true;
+}
+
+bool RemizovKMaxInMatrixStringSEQ::PostProcessingImpl() {
+  return GetOutput().size() == GetInput().size();
 }
 
 }  // namespace remizov_k_max_in_matrix_string
