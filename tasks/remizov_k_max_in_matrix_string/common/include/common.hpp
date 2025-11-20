a58c0f519d85c10f75ac9c0489f581556874d4c3
Kirill <remizov.kirill2005@yandex.ru>
2025-11-20 23:32:43 +0300

    fix(Common): Explicit definition of a test type

--- tasks/remizov_k_max_in_matrix_string/common/include/common.hpp
+++ tasks/remizov_k_max_in_matrix_string/common/include/common.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <string>
 #include <tuple>
 #include <vector>
 
@@ -9,7 +10,7 @@ namespace remizov_k_max_in_matrix_string {
 
 using InType = std::vector<std::vector<int>>;
 using OutType = std::vector<int>;
-using TestType = std::tuple<InType, OutType>;
+using TestType = std::tuple<std::vector<std::vector<int>>, std::vector<int>>;
 using BaseTask = ppc::task::Task<InType, OutType>;
 
 }  // namespace remizov_k_max_in_matrix_string
